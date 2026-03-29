05fa01c8cab827d283875569e0669d0bd4f78b56
Devesh Wadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2026-03-30 00:21:55 +0530

    Change email transport configuration to use port 587

--- backend/config/email.config.js
+++ backend/config/email.config.js
@@ -8,8 +8,8 @@ dns.setDefaultResultOrder("ipv4first");
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.hostinger.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
