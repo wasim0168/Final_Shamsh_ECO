c6ea9eae3b69129c0bc257dccda5cce0454bc0a9
Wasim Sheikh <154791978+wasim0168@users.noreply.github.com>
2026-09-23 19:34:08 +0530

    Integrate dotenv for database configuration
    Added dotenv to load environment variables for database connection.

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,10 @@
 import { connect } from "mongoose";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const connectDb = async () => {
+    console.log((process.env.DATABASE_URL);
     try {
         await connect(process.env.DATABASE_URL);
         console.log("Database connected successfully");
