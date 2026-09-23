2090aa55c4cad442df186712dbe14fc2a8f9e9fd
Wasim Sheikh <154791978+wasim0168@users.noreply.github.com>
2026-09-23 19:37:13 +0530

    Uncomment database URL logging

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const connectDb = async () => {
-    // console.log(process.env.DATABASE_URL);
+    console.log(process.env.DATABASE_URL);
     try {
         await connect(process.env.DATABASE_URL);
         console.log("Database connected successfully");
